f9a01544f42423ef4c89f8f5e29ad1c20af36f03
jblasek <jacob.blasek1220@gmail.com>
2018-09-05 21:24:57 -0400

    I came home from school and my dog was still not a dog

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        
+        Tester funcs = new Tester();
+
+        int a = funcs.equationOne();
+        int b = funcs.equationTwo();
+        int c = funcs.equationThree();
+
+        System.out.println("79 + 3 * (4+ 82 - 68) - 7 + 19 = " + a);
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + b);
+        System.out.println("10359 * 56 * 11 + 2246 = " + c);
     }
 }
---
+++ src/com/company/Tester.java
@@ -0,0 +1,15 @@
+package com.company;
+
+class Tester {
+    int equationOne(){
+        return 79 + 3 * (4 + 82 - 68) - 7 + 19;
+    }
+
+    int equationTwo(){
+        return (179 + 21 + 10) / 7 + 181;
+    }
+
+    int equationThree(){
+        return 10389 * 56 * 11 + 2246;
+    }
+}
